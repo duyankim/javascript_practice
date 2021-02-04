5798ecf78f2e509f281a47874666a0c69e29f931
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-02-04 17:38:18 +0900

    smooth scroll 구현

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-function scrollAppear() {
-  var introText = document.querySelector(".intro-text");
-  var introPosition = introText.getBoundingClientRect().top;
-  //introText의 위치와 뷰포트의 최상단 위치의 거리차를 알려준다.
-  var screenPosition = window.innerHeight / 1.2;
-  //뷰포트의 높이를 찾는다. 1.2로 나눈 이유는 뷰포트의 일정 높이로 스크롤 했을 때 나타나게 하기 위함이다.
+function smoothScroll(target, duration) {
+  var targetItem = document.querySelector(target);
+  var targetPosition = targetItem.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
 
-  if (introPosition < screenPosition) {
-    introText.classList.add("intro-appear");
+  function animationScroll(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, targetPosition, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animationScroll);
   }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animationScroll);
 }
 
-window.addEventListener("scroll", scrollAppear);
-//스크롤 할 때 효과과 나타나도록 window scroll에 이벤트를 넣는다.
+var section1 = document.querySelector(".section1");
+var section2 = document.querySelector(".section2");
+
+section1.addEventListener("click", (e) => {
+  e.preventDefault();
+  smoothScroll(".section2", 2000);
+});
+
+section2.addEventListener("click", (e) => {
+  e.preventDefault();
+  smoothScroll(".section1", 2000);
+});
